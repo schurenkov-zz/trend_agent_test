fd4e333cc23943c1c75a057536daa2d613df0910
SILENTIUM <mr.schurenkov@gmail.com>
2018-07-13 15:25:25 +0300

    fix

--- src/api/main-api.js
+++ src/api/main-api.js
@@ -5,6 +5,6 @@ import qs from 'qs';
 const API = '00ebbff799ab7d176c40a1e8d74600cc';
 
 export function getWeather(){
-  return axios.get('http://api.openweathermap.org/data/2.5/find?lat=55.7&lon=37.6&units=metric&lang=ru&cnt=10&APPID=' + API)
+  return axios.get('https://api.openweathermap.org/data/2.5/find?lat=55.7&lon=37.6&units=metric&lang=ru&cnt=10&APPID=' + API)
               .then(result=> { return result.data })
 }
